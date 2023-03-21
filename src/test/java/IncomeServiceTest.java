e12b01049aa4a12dc5931c3c3bd9fb9bf7b6cfa0
Arthur Petrosov <arthurpetrosov@gmail.com>
2023-03-21 22:54:33 +0400

    Csv commit

--- src/test/java/IncomeServiceTest.java
+++ src/test/java/IncomeServiceTest.java
@@ -1,31 +1,19 @@
 import org.example.FrilanceService.services.IncomeService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class IncomeServiceTest {
 
-    @Test
-    void colc1version1() {
+    @ParameterizedTest
+    @CsvFileSource(files="src/test/resources/IncomeServiceDate.csv")
+    public void colc1version1(int income, int expenses, int threshold,int expected) {
         IncomeService service = new IncomeService();
 
-        // подготавливаем данные:
-        int expected = 3;
-
-        int actual = service.calculate(10_000, 3_000, 20_000);
+        int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
-
-    @Test
-    void colc1version2() {
-        IncomeService service = new IncomeService();
-
-        // подготавливаем данные:
-        int expected = 2;
-
-        int actual = service.calculate(100_000, 60_000, 150_000);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
 }
